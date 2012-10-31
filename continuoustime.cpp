5d88a8610dc12d0a35142ac44fff7b7bbbf39e91
Mauro Iazzi <mauro.iazzi@gmail.com>
2012-10-31 17:12:46 +0100

    fill code for the CT Monte-Carlo

--- continuoustime.cpp
+++ continuoustime.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+#include <map>
 #include <random>
 #include <chrono>
 #include <functional>
@@ -24,7 +25,7 @@ extern "C" void dggev_ (const char *jobvl, const char *jobvr, const int &N,
 			double *vl, const int &ldvl, double *vr, const int &ldvr,
 			double *work, const int &lwork, int &info);
 
-void ddgev (const Eigen::MatrixXd &A, const Eigen::MatrixXd &B, Eigen::VectorXcd &alpha, Eigen::VectorXd &beta) {
+void dggev (const Eigen::MatrixXd &A, const Eigen::MatrixXd &B, Eigen::VectorXcd &alpha, Eigen::VectorXd &beta) {
 	Eigen::MatrixXd a = A, b = B;
 	int info = 0;
 	int N = a.rows();
@@ -59,35 +60,28 @@ class Configuration {
 	int L; // size of the system
 	int D; // dimension
 	int V; // volume of the system
-	int N; // number of time-steps
 	double beta; // inverse temperature
-	double dt; // time step 
 	double g; // interaction strength
 	double mu; // chemical potential
-	double A; // sqrt(exp(g*dt)-1)
+	double A; // sqrt(g)
 	double B; // magnetic field
 	double J; // next-nearest neighbour hopping
 
-	std::vector<Eigen::VectorXd> diagonals;
+	std::map<double, Eigen::VectorXd> diagonals;
 
 	std::default_random_engine generator;
 	std::bernoulli_distribution distribution;
-	std::uniform_int_distribution<int> randomPosition;
-	std::uniform_int_distribution<int> randomTime;
+	std::uniform_real_distribution<double> randomDouble;
+	std::uniform_real_distribution<double> randomTime;
 	std::exponential_distribution<double> trialDistribution;
 
 	Eigen::VectorXd energies;
-	Eigen::VectorXd freePropagator;
 
 	Eigen::MatrixXd positionSpace; // current matrix in position space
 	Eigen::MatrixXcd momentumSpace;
 
 	fftw_plan x2p;
 	fftw_plan p2x;
-
-	Eigen::FullPivHouseholderQR<Eigen::MatrixXd> decomposer;
-
-	//Eigen::MatrixXcd n_s; // single particle density matrix
 
 	double plog;
 
@@ -100,22 +94,20 @@ class Configuration {
 	double n_up;
 	double n_dn;
 
-	int qrnumber;
-
 	public:
 
-	Configuration (int d, int l, int n, double Beta, double interaction, double m, double b, double j)
-		: L(l), D(d), V(std::pow(l, D)), N(n), beta(Beta), dt(Beta/n),
-		g(interaction), mu(m), B(b), J(j), qrnumber(0), distribution(0.5), randomPosition(0, l-1),
-		randomTime(0, n-1), trialDistribution(1.0) {
-		A = sqrt(exp(g*dt)-1.0);
+	Configuration (int d, int l, double Beta, double interaction, double m, double b, double j)
+		: L(l), D(d), V(std::pow(l, D)), beta(Beta),
+		g(interaction), mu(m), B(b), J(j), distribution(0.5), randomDouble(1.0),
+		randomTime(0, Beta), trialDistribution(1.0) {
+		A = sqrt(g);
 		auto distributor = std::bind(distribution, generator);
-		diagonals.insert(diagonals.begin(), N, Eigen::VectorXd::Zero(V));
-		for (int i=0;i<diagonals.size();i++) {
-			for (int j=0;j<V;j++) {
-				diagonals[i][j] = distributor()?A:-A;
-			}
-		}
+		//diagonals.insert(diagonals.begin(), N, Eigen::VectorXd::Zero(V));
+		//for (int i=0;i<diagonals.size();i++) {
+			//for (int j=0;j<V;j++) {
+				//diagonals[i][j] = distributor()?A:-A;
+			//}
+		//}
 		positionSpace = Eigen::MatrixXd::Identity(V, V);
 		momentumSpace = Eigen::MatrixXcd::Identity(V, V);
 
@@ -129,143 +121,71 @@ class Configuration {
 		momentumSpace = Eigen::MatrixXcd::Identity(V, V);
 
 		energies = Eigen::VectorXd::Zero(V);
-		freePropagator = Eigen::VectorXd::Zero(V);
 		for (int i=0;i<V;i++) {
 			energies[i] = - cos(2.0*(i%L)*pi/L) - cos(2.0*((i/L)%L)*pi/L) - cos(2.0*(i/L/L)*pi/L) + (3-D) - mu;
 			energies[i] += J * (- cos(4.0*(i%L)*pi/L) - cos(4.0*((i/L)%L)*pi/L) - cos(4.0*(i/L/L)*pi/L) + (3-D) );
-			freePropagator[i] = exp(-dt*energies[i]);
 		}
 
 		plog = logProbability();
 	}
 
-	void checkConsistency () {
-		Eigen::HouseholderQR<Eigen::MatrixXd> qrsolver;
-		Eigen::MatrixXd R = Eigen::MatrixXd::Identity(V, V);
-		int qrperiod = qrnumber>0?N/qrnumber:0;
-
-		positionSpace.setIdentity(V, V);
-		R.setIdentity(V, V);
-		for (int i=0;i<N;i++) {
-			positionSpace.applyOnTheLeft((Eigen::VectorXd::Constant(V, 1.0)+diagonals[i]).asDiagonal());
-			Eigen::ArrayXcd ev = (positionSpace*R).eigenvalues();
-			if (std::cos(ev.log().sum().imag())<0.999) {
-				std::cout << "imaginary part in the propagator at time slice " << i << std::endl;
-				std::cout << positionSpace*R << std::endl << std::endl;
-				std::cout << ev.transpose() << std::endl;
-				std::cout << (Eigen::VectorXd::Constant(V, 1.0)+diagonals[i]).transpose() << std::endl << std::endl;
-			} else {
-				std::cout << "everything fine up to " << i << std::endl;
-				std::cout << positionSpace*R << std::endl << std::endl;
-				std::cout << ev.transpose() << std::endl << std::endl;
-			}
-			fftw_execute(x2p);
-			momentumSpace.applyOnTheLeft(freePropagator.asDiagonal());
-			fftw_execute(p2x);
-			positionSpace /= V;
-			ev = (positionSpace*R).eigenvalues();
-			if (std::cos(ev.log().sum().imag())<0.999) {
-				std::cout << "imaginary part in the propagator at time slice " << i << std::endl;
-				std::cout << positionSpace*R << std::endl << std::endl;
-				std::cout << ev.transpose() << std::endl;
-				std::cout << (Eigen::VectorXd::Constant(V, 1.0)+diagonals[i]).transpose() << std::endl << std::endl;
-			} else {
-				std::cout << "everything fine up to " << i << std::endl;
-				std::cout << positionSpace*R << std::endl << std::endl;
-				std::cout << ev.transpose() << std::endl << std::endl;
-			}
-			if ((qrperiod>0 && (i+1)%qrperiod==0) || i==N-1) {
-				qrsolver.compute(positionSpace);
-				R.applyOnTheLeft(qrsolver.householderQ().inverse()*positionSpace);
-				Eigen::VectorXd sign = Eigen::VectorXd::Ones(V);
-				for (int k=0;k<V;k++) if (R(k, k)<0.0) sign[k] = -1.0;
-				R.applyOnTheLeft(sign.asDiagonal());
-				positionSpace = qrsolver.householderQ();
-				positionSpace.applyOnTheRight(sign.asDiagonal());
-			}
-		}
-		Eigen::VectorXcd eva;
-		Eigen::VectorXd evb;
-		ddgev(R, positionSpace.transpose(), eva, evb);
-
-		//positionSpace.applyOnTheRight(R);
-		if (std::cos(eva.array().log().sum().imag() - evb.array().log().sum())<0.999) {
-			std::cout << "imaginary part in the propagator " << std::endl;
-			std::cout << positionSpace << std::endl << std::endl;
-			std::cout << positionSpace.eigenvalues().transpose() << std::endl << std::endl;
-		} else {
-			std::cout << "generalized ev problem was ok" << std::endl;
-		}
-	}
-
 	double logProbability () {
-		Eigen::HouseholderQR<Eigen::MatrixXd> qrsolver;
-		Eigen::MatrixXd R = Eigen::MatrixXd::Identity(V, V);
-		int qrperiod = qrnumber>0?N/qrnumber:0;
-
+		const double F = sqrt(2.0)/2.0;
+		double t = 0.0;
+		double dt;
 		positionSpace.setIdentity(V, V);
-		R.setIdentity(V, V);
-		for (int i=0;i<N;i++) {
-			positionSpace.applyOnTheLeft((Eigen::VectorXd::Constant(V, 1.0)+diagonals[i]).asDiagonal());
+		for (auto i : diagonals) {
 			fftw_execute(x2p);
-			momentumSpace.applyOnTheLeft(freePropagator.asDiagonal());
+			dt = i.first-t;
+			momentumSpace.applyOnTheLeft((-dt*energies).array().exp().matrix().asDiagonal());
 			fftw_execute(p2x);
 			positionSpace /= V;
-			if ((qrperiod>0 && (i+1)%qrperiod==0) || i==N-1) {
-				qrsolver.compute(positionSpace);
-				R.applyOnTheLeft(qrsolver.householderQ().inverse()*positionSpace);
-				positionSpace = qrsolver.householderQ();
-			}
+			// FIXME needs to take into account 1/2 from the sum??
+			positionSpace.applyOnTheLeft((Eigen::VectorXd::Constant(V, 1.0)+i.second).asDiagonal());
+			t = i.first;
 		}
-		Eigen::VectorXcd eva;
-		Eigen::VectorXd evb;
-		ddgev(R, positionSpace.transpose(), eva, evb);
-
-		positionSpace.applyOnTheRight(R);
+		fftw_execute(x2p);
+		dt = beta-t;
+		momentumSpace.applyOnTheLeft((-dt*energies).array().exp().matrix().asDiagonal());
+		fftw_execute(p2x);
+		positionSpace /= V;
 
 		//Eigen::MatrixXd S1 = Eigen::MatrixXd::Identity(V, V) + std::exp(+beta*B)*positionSpace;
 		//Eigen::MatrixXd S2 = Eigen::MatrixXd::Identity(V, V) + std::exp(-beta*B)*positionSpace;
 
-		std::complex<double> ret = 0.0;
-		//ret += (1.0 + std::exp(+beta*B)*positionSpace.eigenvalues().array()).log().sum();
-		//ret += (1.0 + std::exp(-beta*B)*positionSpace.eigenvalues().array()).log().sum();
-		ret += (evb.cast<std::complex<double>>() + std::exp(+beta*B)*eva).array().log().sum();
-		ret -= evb.array().log().sum();
-		ret += (evb.cast<std::complex<double>>() + std::exp(-beta*B)*eva).array().log().sum();
-		ret -= evb.array().log().sum();
+		Eigen::ArrayXcd ev = positionSpace.eigenvalues();
 
-		Eigen::ArrayXcd temp = eva.array()/evb.array().cast<std::complex<double>>();
-		//std::cerr << temp.transpose() << std::endl;
-		//std::cerr << positionSpace.eigenvalues().transpose() << std::endl << std::endl;
+		std::complex<double> ret = 0.0;
+		ret += ( 1.0 + ev*std::exp(-beta*B) ).log().sum() + ( 1.0 + ev*std::exp(+beta*B) ).log().sum();
 
 		if (std::cos(ret.imag())<0.99) {
-			if (qrnumber==N) {
-				checkConsistency();
-				throw("wtf");
-			} else {
-				qrnumber++;
-				std::cerr << "imaginary part = " << ret.imag() << " increasing qrnumber -> " << qrnumber << std::endl;
-				return logProbability();
-			}
-			return logProbability();
+			Eigen::MatrixXd S1 = Eigen::MatrixXd::Identity(V, V) + std::exp(+beta*B)*positionSpace;
+			Eigen::MatrixXd S2 = Eigen::MatrixXd::Identity(V, V) + std::exp(-beta*B)*positionSpace;
+			std::cerr << positionSpace << std::endl << std::endl;
+			std::cerr << S1 << std::endl << std::endl;
+			std::cerr << S1.eigenvalues().transpose() << std::endl;
+			std::cerr << S2.eigenvalues().transpose() << std::endl;
+			std::cerr << S1.eigenvalues().array().log().sum() << std::endl;
+			std::cerr << S2.eigenvalues().array().log().sum() << std::endl;
+			std::cerr << ev.transpose() << std::endl;
+			std::cerr << ret << std::endl;
+			std::cerr << diagonals.size() << std::endl;
+			throw("wtf");
 		}
 		return ret.real();
 	}
 
-	void print () {
-		for (int i=0;i<N;i++) {
-			for (int j=0;j<V;j++) {
-				std::cout << (diagonals[i][j]<0?'-':'+');
-			}
-			std::cout << std::endl;
-		}
-	}
-
-	bool metropolis (int M) {
+	bool metropolisFlip (int M) {
+		if (diagonals.size()==0) return false;
 		bool ret = false;
+		if (M>V) M = V;
 		std::vector<int> index(M);
+		int t = std::uniform_int_distribution<int>(0, diagonals.size()-1)(generator);
+		std::map<double, Eigen::VectorXd>::iterator diter = diagonals.begin();
+		while (t--) diter++;
+		Eigen::VectorXd &d = (*diter).second;
 		for (int j=0;j<M;j++) {
-			std::uniform_int_distribution<int> distr(0, V*N-j-1);
+			std::uniform_int_distribution<int> distr(0, V-j-1);
 			int x = distr(generator);
 			int i = j;
 			// standard insertion sort would be:
@@ -281,36 +201,94 @@ class Configuration {
 			index[i] = x+i;
 		}
 		for (int i=0;i<M;i++) {
-			int t = index[i]/V;
 			int x = index[i]%V;
-			diagonals[t][x] = -diagonals[t][x];
+			d[x] = -d[x];
 		}
 		double trial = logProbability();
 		if (-trialDistribution(generator)<trial-plog) {
-			//std::cout << "accepted " << trial-plog << std::endl;
 			plog = trial;
-			//density = std::valarray<double>(n_s.diagonal().real().data(), V);
-			n_up = ( Eigen::MatrixXd::Identity(V, V) - (Eigen::MatrixXd::Identity(V, V) + exp(+beta*B) * positionSpace).inverse() ).trace();
-			n_dn = ( Eigen::MatrixXd::Identity(V, V) - (Eigen::MatrixXd::Identity(V, V) + exp(-beta*B) * positionSpace).inverse() ).trace();
-			//number = n_s.real().trace();
+			n_up = ( Eigen::MatrixXd::Identity(V, V) - sqrt(2.0)*(sqrt(2)*Eigen::MatrixXd::Identity(V, V) + exp(+beta*B) * positionSpace).inverse() ).trace();
+			n_dn = ( Eigen::MatrixXd::Identity(V, V) - sqrt(2.0)*(sqrt(2)*Eigen::MatrixXd::Identity(V, V) + exp(-beta*B) * positionSpace).inverse() ).trace();
 			ret = true;
 		} else {
-			//std::cout << "rejected " << trial-plog << std::endl;
 			for (int i=0;i<M;i++) {
-				int t = index[i]/V;
 				int x = index[i]%V;
-				diagonals[t][x] = -diagonals[t][x];
+				d[x] = -d[x];
 			}
 			ret = false;
 		}
-		//std::cout << n_up << ' ' << n_dn << std::endl;
-		//measuredNumber << number;
-		//std::cout << measuredNumber << std::endl;
 		return ret;
 	}
 
-	void setQRNumber (int n) {
-		qrnumber = n;
+	bool metropolisUp () {
+		bool ret = false;
+		double t = randomTime(generator);
+		auto distributor = std::bind(distribution, generator);
+		std::map<double, Eigen::VectorXd>::iterator diter = diagonals.find(t);
+		if (diter!=diagonals.end()) return false;
+		diagonals[t] = Eigen::VectorXd::Zero(V);
+		diter = diagonals.find(t);
+		for (int i=0;i<V;i++) diter->second[i] = distributor()?A:-A;
+		double trial = logProbability();
+		if (randomDouble(generator)<std::exp(trial-plog)*beta/diagonals.size()) {
+			//std::cerr << "accepted increase: time steps = " << diagonals.size() << std::endl;
+			plog = trial;
+			n_up = ( Eigen::MatrixXd::Identity(V, V) - sqrt(2.0)*(sqrt(2)*Eigen::MatrixXd::Identity(V, V) + exp(+beta*B) * positionSpace).inverse() ).trace();
+			n_dn = ( Eigen::MatrixXd::Identity(V, V) - sqrt(2.0)*(sqrt(2)*Eigen::MatrixXd::Identity(V, V) + exp(-beta*B) * positionSpace).inverse() ).trace();
+			ret = true;
+		} else {
+			diagonals.erase(diter);
+			ret = false;
+		}
+		return ret;
+	}
+
+	bool metropolisDown () {
+		bool ret = false;
+		std::pair<double, Eigen::VectorXd> store;
+		int t = std::uniform_int_distribution<int>(0, diagonals.size()-1)(generator);
+		std::map<double, Eigen::VectorXd>::iterator diter = diagonals.begin();
+		while (t--) diter++;
+		store = *diter;
+		diagonals.erase(diter);
+		double trial = logProbability();
+		if (randomDouble(generator)<std::exp(trial-plog)*(diagonals.size()+1)/beta) {
+			//std::cerr << "accepted decrease: time steps = " << diagonals.size() << std::endl;
+			plog = trial;
+			n_up = ( Eigen::MatrixXd::Identity(V, V) - sqrt(2.0)*(sqrt(2)*Eigen::MatrixXd::Identity(V, V) + exp(+beta*B) * positionSpace).inverse() ).trace();
+			n_dn = ( Eigen::MatrixXd::Identity(V, V) - sqrt(2.0)*(sqrt(2)*Eigen::MatrixXd::Identity(V, V) + exp(-beta*B) * positionSpace).inverse() ).trace();
+			ret = true;
+		} else {
+			diagonals[store.first] = store.second;
+			ret = false;
+		}
+		return ret;
+	}
+
+	bool metropolis (int M) {
+		std::discrete_distribution<int> distribution { 0.90, 0.05, 0.05 };
+		int type = distribution(generator);
+		if (type == 0 || (type == 1 && diagonals.size()==0 )) {
+			return metropolisFlip(M);
+		} else if (type == 1) {
+			//std::cerr << "proposed decrease" << std::endl;
+			return metropolisDown();
+		} else {
+			//std::cerr << "proposed increase" << std::endl;
+			return metropolisUp();
+		}
+	}
+
+	int sliceNumber () { return diagonals.size(); }
+
+	void print () {
+		for (auto i : diagonals) {
+			std::cout << i.first << '\t';
+			for (int j=0;j<V;j++) {
+				std::cout << (i.second[j]<0?'-':'+');
+			}
+			std::cout << std::endl;
+		}
 	}
 
 	~Configuration () { fftw_destroy_plan(x2p); fftw_destroy_plan(p2x); }
@@ -320,8 +298,7 @@ class Configuration {
 int main (int argc, char **argv) {
 	int D = 1;
 	int L = 4;
-	int N = 1000;
-	int M = 500;
+	int M = 1;
 	double beta = 10.0;
 	double g = 0.1;
 	double mu = -0.5;
@@ -339,9 +316,6 @@ int main (int argc, char **argv) {
 					break;
 				case 'L':
 					L = atoi(argv[++i]);
-					break;
-				case 'N':
-					N = atoi(argv[++i]);
 					break;
 				case 'M':
 					M = atoi(argv[++i]);
@@ -367,8 +341,7 @@ int main (int argc, char **argv) {
 			}
 		}
 	}
-	Configuration configuration(D, L, N, beta, g, mu, B, J);
-	configuration.setQRNumber(qrn);
+	Configuration configuration(D, L, beta, g, mu, B, J);
 
 	int n = 0;
 	int a = 0;
@@ -377,8 +350,6 @@ int main (int argc, char **argv) {
 		configuration.metropolis(M);
 	}
 
-	density.set_bin_size(128);
-	magnetization.set_bin_size(128);
 	std::chrono::steady_clock::time_point time_start = std::chrono::steady_clock::now();
 	std::chrono::steady_clock::time_point time_end = std::chrono::steady_clock::now();
 	for (;;) {
@@ -386,35 +357,22 @@ int main (int argc, char **argv) {
 		n++;
 		density << configuration.n_up + configuration.n_dn;
 		magnetization << configuration.n_up - configuration.n_dn;
-		if (n%1024==0) {
+		if (n%(1<<10)==0) {
 			time_end = std::chrono::steady_clock::now();
 			std::cout << "dimension = " << D << ", size = " << L << std::endl;
-			std::cout << "time steps = " << N << ", decompositions = " << configuration.qrnumber << std::endl;
 			std::cout << "temperature = " << (1.0/beta) << ", interaction = " << g << std::endl;
 			std::cout << "chemical potential = " << mu << ", magnetic field = " << B << std::endl;
 			std::cout << "acceptance = " << (double(a)/double(n)) << " spin flips = " << M << std::endl;
 			std::cout << "elapsed: " << std::chrono::duration_cast<std::chrono::duration<double>>(time_end - time_start).count() << " seconds" << std::endl;
 			std::cout << "steps per second = " << n/std::chrono::duration_cast<std::chrono::duration<double>>(time_end - time_start).count() << std::endl;
+			std::cout << "slices = " << configuration.sliceNumber() << std::endl;
 			std::cout << density << std::endl;
 			std::cout << magnetization << std::endl;
+			//configuration.print();
 			if (a>0.6*n) {
-				M += 1;
-				time_start = std::chrono::steady_clock::now();
 				//configuration.measuredNumber.reset(true);
 				//configuration.eigenvalues.reset(true);
-				density.reset(true);
-				magnetization.reset(true);
-				density.set_bin_size(128);
-				magnetization.set_bin_size(128);
-				n = 0;
-				a = 0;
 			} else if (a<0.4*n) {
-				density.reset(true);
-				magnetization.reset(true);
-				density.set_bin_size(128);
-				magnetization.set_bin_size(128);
-				M -= 1;
-				M = M>0?M:1;
 			}
 
 		}
