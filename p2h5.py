81a3b1fa4ace4950ba4c1ef81738a574b318502a
Mauro Iazzi <mauro.iazzi@gmail.com>
2012-12-21 16:46:24 +0100

    different test parameters

--- p2h5.py
+++ p2h5.py
@@ -3,16 +3,16 @@
 import pyalps.hdf5 as h5
  
 ar = h5.oArchive('test.h5')
-ar.write("/parameters/L", 2)
-ar.write("/parameters/D", 2)
-ar.write("/parameters/T", 0.01)
-ar.write("/parameters/g", 0.4)
-ar.write("/parameters/mu", 0.0)
+ar.write("/parameters/L", 1)
+ar.write("/parameters/D", 1)
+ar.write("/parameters/T", 0.1)
+ar.write("/parameters/g", 0.3)
+ar.write("/parameters/mu", 0)
 ar.write("/parameters/B", 0.0)
-ar.write("/parameters/t", 0.1)
+ar.write("/parameters/t", 1)
 ar.write("/parameters/J", 0.0)
 
 ar.write("/parameters/THERMALIZATION", 100000)
-ar.write("/parameters/SWEEPS", 100000)
+ar.write("/parameters/SWEEPS", 200000)
 
 
