1a8b481ef4e830f2fd51c4ee22169bcc6b87c67f
Mauro Iazzi <mauro.iazzi@gmail.com>
2013-02-18 13:43:42 +0100

    changed config

--- config.lua
+++ config.lua
@@ -9,24 +9,26 @@ local tasks = setmetatable({}, { __index=table })
 
 local t = 0.2
 local L = 4;
+--local seed = os.time()
+local seed = 42
 for x = 0.1, 1.1, 0.1 do
 	tasks:insert( flip_params{
-		Lx = 4,
-		Ly = 4,
+		Lx = 8,
+		Ly = 8,
 		Lz = 1,
 		T = x*t,
 		N = 100/x,
-		tx = t,
-		ty = 0.1*t,
-		tz = 0.1*t,
+		tx = 1*t,
+		ty = 1*t,
+		tz = 1*t,
 		U = 4*t,
 		mu = 0.0,
 		B = 0.0,
 		THREADS = 1,
 		THERMALIZATION = 10000,
-		SWEEPS = 100000,
-		SEED = 42,
-		OUTPUT = 'U4_4x4_T'..tostring(x)..'_.dat',
+		SWEEPS = 10000,
+		SEED = seed,
+		OUTPUT = 'U4_L8_T'..tostring(x)..'_last.dat',
 	} )
 end
 
