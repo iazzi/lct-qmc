bcfa3b0e8ee5c25415d320a60ce9d84314cd1e8d
Mauro Iazzi <mauro.iazzi@gmail.com>
2013-05-08 14:05:39 +0200

    typedef for 2D arrays

--- types.hpp
+++ types.hpp
@@ -15,6 +15,7 @@ typedef std::complex<Real> Complex;
 
 typedef Eigen::Matrix<Real, Eigen::Dynamic, Eigen::Dynamic> Matrix_d;
 typedef Eigen::Array<Real, Eigen::Dynamic, 1> Array_d;
+typedef Eigen::Array<Real, Eigen::Dynamic, Eigen::Dynamic> Array_dd;
 typedef Eigen::Matrix<Real, Eigen::Dynamic, 1> Vector_d;
 typedef Eigen::Matrix<Complex, Eigen::Dynamic, 1> Vector_cd;
 
