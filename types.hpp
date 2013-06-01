9bb0dc96517b74c6230a59ad5f7ad6476bcb3d08
Mauro Iazzi <mauro.iazzi@gmail.com>
2013-06-01 23:32:03 +0200

    define complex matrix. only allow doubles

--- types.hpp
+++ types.hpp
@@ -3,17 +3,12 @@
 
 //#define USE_LONG_DOUBLE
 
-#ifdef USE_LONG_DOUBLE
-typedef long double Real;
-#elif USE_FLOAT
-typedef float Real;
-#else
 typedef double Real;
-#endif
 
 typedef std::complex<Real> Complex;
 
 typedef Eigen::Matrix<Real, Eigen::Dynamic, Eigen::Dynamic> Matrix_d;
+typedef Eigen::Matrix<Complex, Eigen::Dynamic, Eigen::Dynamic> Matrix_cd;
 typedef Eigen::Array<Real, Eigen::Dynamic, 1> Array_d;
 typedef Eigen::Array<Real, Eigen::Dynamic, Eigen::Dynamic> Array_dd;
 typedef Eigen::Matrix<Real, Eigen::Dynamic, 1> Vector_d;
