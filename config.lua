ed8b3a8bdf7114d380039e95de52750345863756
Mauro Iazzi <mauro.iazzi@gmail.com>
2013-06-01 23:31:31 +0200

    use range interators

--- config.lua
+++ config.lua
@@ -1,5 +1,15 @@
 #!/usr/bin/lua
 
+local function range (a, b, N)
+	local i = 0
+	return function ()
+		if i>N then return nil end
+		local ret = a+i*(b-a)/N
+		i = i+1
+		return ret
+	end
+end
+
 local function flip_params (t)
 	t.U, t.B, t.mu = -t.U, 2.0*t.mu-t.U, 0.5*(t.B-t.U)
 	return t
@@ -7,7 +17,7 @@ end
 
 local tasks = setmetatable({}, { __index=table })
 
-local t = 0.3
+local t = 0.2
 local U = 4*t
 local tx, ty, tz = t, t, t
 local J = 4*t*t/U
@@ -17,24 +27,23 @@ local seed = os.time()
 --local _, threads = (os.getenv("LSB_HOSTS") or ''):gsub("(%S+)", "%1")
 --if threads<1 then threads = 1 end
 --print("using "..threads.." threads")
---tasks.THREADS = threads
+--tasks.THREADS = 48
 
 local mu_min, mu_max = -2*(tx+ty+tz), U/2
-local d_mu = (mu_max-mu_min)/30
 
-for y = mu_max, mu_max, -d_mu do
-	for x = 0.2, 0.8, 0.05 do
-		for _ = 1, 50 do
+for y in range(mu_max, mu_max, 30) do
+	for x in range(0.8, 1.1, 20) do
+		for _ = 1, 100 do
 			seed = seed + 127
 			tasks:insert( flip_params{
-				Lx = 8,
-				Ly = 8,
-				Lz = 2,
+				Lx = 6,
+				Ly = 6,
+				Lz = 6,
 				T = x*t,
 				N = 10/x,
-				tx = tx,
-				ty = ty,
-				tz = tz,
+				tx = 1.0*tx,
+				ty = 1.0*ty,
+				tz = 1.0*tz,
 				U = U,
 				mu = y,
 				B = 0.0,
@@ -42,9 +51,9 @@ for y = mu_max, mu_max, -d_mu do
 				THERMALIZATION = 10000,
 				SWEEPS = 100000,
 				SEED = seed,
-				OUTPUT = 's_p_',
+				OUTPUT = 'svd_',
 				--REWEIGHT = 0,
-				SVDPERIOD = 10,
+				SVDPERIOD = 30,
 			} )
 		end
 	end
