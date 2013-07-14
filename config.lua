8fc65e0c0a78962a8fdefa1073a7a1e9b16ed088
Mauro Iazzi <mauro.iazzi@gmail.com>
2013-07-14 14:22:56 +0200

    config change for SVD

--- config.lua
+++ config.lua
@@ -2,7 +2,8 @@
 
 local function range (a, b, N)
 	local i = 0
-	if a==b then i,N=1,1 end
+	if N<0 then a, b, N = b, a, -N end
+	if a==b then i, N = 1, 1 end
 	return function ()
 		if i>N then return nil end
 		local ret = a+i*(b-a)/N
@@ -16,29 +17,34 @@ local function flip_params (t)
 	return t
 end
 
+local function copy (t)
+	local ret = {}
+	for k, v in pairs(t) do ret[k] = v end
+	return ret
+end
+
 local tasks = setmetatable({}, { __index=table })
 
-local t = 0.2
+local t = 0.1
 local U = 4*t
 local tx, ty, tz = t, t, t
 local J = 4*t*t/U
 local L = 4;
-local seed = os.time()
+local seed = 22222 -- os.time()
 
 --local _, threads = (os.getenv("LSB_HOSTS") or ''):gsub("(%S+)", "%1")
 --if threads<1 then threads = 1 end
 --print("using "..threads.." threads")
 --tasks.THREADS = 48
 
-local mu_min, mu_max = -2*(tx+ty+tz), U/2
+local mu_min, mu_max = -0.5*(tx+ty+tz), U/2
 
-for y in range(mu_max, mu_max, 30) do
-	for x in range(0.8, 1.1, 30) do
-		for _ = 1, 100 do
+for y in range(mu_max, mu_min, -30) do
+	for x in range(0.1, 1.2, 30) do
 			seed = seed + 127
 			tasks:insert( flip_params{
-				Lx = 16,
-				Ly = 1,
+				Lx = 8,
+				Ly = 8,
 				Lz = 1,
 				T = x*t,
 				N = 10/x,
@@ -48,17 +54,20 @@ for y in range(mu_max, mu_max, 30) do
 				U = U,
 				mu = y,
 				B = 0.0,
-				h = 2.0e-3,
-				THERMALIZATION = 10000,
-				SWEEPS = 100000,
+				h = 0.0e-2,
+				THERMALIZATION = 1000,
+				SWEEPS = 10000,
 				SEED = seed,
-				OUTPUT = 'svd_',
+				OUTPUT = 'unstable_',
 				--REWEIGHT = 0,
-				SVDPERIOD = 30,
+				SLICES = 5,
+				SVD = 1,
+				TIMES = 1,
 			} )
-		end
 	end
 end
+
+tasks.dir = "unstable_dir"
 
 return tasks
 
