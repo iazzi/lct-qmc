9de245752689bf7bd8f6daccaf44a7603d1ff1f5
Mauro Iazzi <mauro.iazzi@gmail.com>
2013-08-23 17:52:17 +0200

    add serialization to config.lua

--- config.lua
+++ config.lua
@@ -26,7 +26,7 @@ end
 local tasks = setmetatable({}, { __index=table })
 
 local t = 0.1
-local U = 7*t
+local U = 4*t
 local tx, ty, tz = t, t, t
 local J = 4*t*t/U
 local L = 4;
@@ -38,10 +38,10 @@ local seed = 33333 -- os.time()
 --tasks.THREADS = 48
 
 local mu_min, mu_max = -2.0*(tx+ty+tz), U/2
-mu_min = mu_max - 0.00*t
+mu_min = mu_max - 1.20*t
 
-for y in range(mu_max, mu_min, -30) do
-	for x in range(0.05, 0.25, 30) do
+for y in range(mu_max, mu_min, 30) do
+	for x in range(0.05, 1.0, 30) do
 			seed = seed + 127
 			tasks:insert( flip_params{
 				Lx = 4,
@@ -56,22 +56,50 @@ for y in range(mu_max, mu_min, -30) do
 				mu = y,
 				B = 0.0,
 				h = 0.0e-2,
-				THERMALIZATION = 1000,
-				SWEEPS = 10000,
+				THERMALIZATION = 30000,
+				SWEEPS = 300000,
 				SEED = seed,
-				OUTPUT = 'unstable_',
+				OUTPUT = 'pair_',
 				--REWEIGHT = 0,
-				SLICES = 20,
-				SVD = 3,
+				SLICES = 10,
+				SVD = 1,
 				TIMES = 1,
 				--update_start = 300,
 				--update_end = 700-1,
-				max_update_size = 7,
+				max_update_size = 64,
 			} )
 	end
 end
 
 tasks.dir = "unstable_dir"
+
+function serialize (f, o)
+	if type(f)=='string' then f = assert(io.open(f, 'w')) end
+	local t = type(o)
+	if t=='number' then
+		f:write(string.format('%a', o))
+	elseif t=='string' then
+		f:write(string.format('%q', o))
+	elseif t=='boolean' then
+		f:write(o and 'true' or 'false')
+	elseif t=='table' then
+		f:write('{\n')
+		for k, v in pairs(o) do
+			if type(k)=='string' and k:match('[_%a][_%w]*') then
+				f:write(' ', k, ' = ')
+			else
+				f:write(' [') serialize(f, k) f:write('] = ')
+			end
+			serialize(f, v)
+			f:write(',\n')
+		end
+		f:write('}\n')
+
+	else
+		io.stderr:write("Unknown type `"..t.."`")
+	end
+end
+
 
 return tasks
 
