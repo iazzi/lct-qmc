006f1fdc5e48f4153712405a59832634706ed115
Mauro Iazzi <mauro.iazzi@gmail.com>
2013-08-28 18:49:06 +0200

    allow globbing in jk.lua

--- jk.lua
+++ jk.lua
@@ -12,11 +12,17 @@ local function transpose (t)
 	return ret
 end
 
-local function average (t)
+local function average (t, w)
 	local sum = 0
 	local n = 0
-	for k, v in pairs(t) do
-		sum, n = sum + v, n + 1
+	if w then
+		for k, v in pairs(t) do
+			sum, n = sum + v/w[k], n + 1.0/w[k]
+		end
+	else
+		for k, v in pairs(t) do
+			sum, n = sum + v, n + 1
+		end
 	end
 	return sum / n
 end
@@ -32,10 +38,20 @@ local function jk (t)
 	end
 	for k, v in pairs(t) do
 	end
-	return math.sqrt(sum2/n - sum*sum/n/n)
+	return math.sqrt(sum2/n - sum*sum/n/n)/math.sqrt(n)
 end
 
+local files = {}
 for _, fn in ipairs{...} do
+	local p = io.popen('ls '..fn)
+	for f in p:lines() do
+		table.insert(files, f)
+		print(f)
+	end
+	p:close()
+end
+local out = {}
+for _, fn in ipairs(files) do
 	--print("doing", fn)
 	local t = {}
 	local f = assert(io.open(fn))
@@ -47,7 +63,7 @@ for _, fn in ipairs{...} do
 				table.insert(values, m)
 			end
 			for i, v in ipairs(values) do
-				values[i] = tonumber(v)
+				values[i] = tonumber(v) or 0.0
 			end
 			local T = table.remove(values, 1)
 			local mu = table.remove(values, 1)
@@ -56,24 +72,24 @@ for _, fn in ipairs{...} do
 			table.insert(t[T][mu], values)
 		end
 	end
-	local out = {}
 	for T, u in pairs(t) do
 		for mu, v in pairs(u) do
 			local w = transpose(v)
 			for i = 1, #w, 2 do
 				local e = jk(w[i])
 				w[i] = average(w[i])
-				w[i+1] = e
+				w[i+1] = math.sqrt(average(w[i+1])/#w[i+1])
+				--w[i+1] = math.sqrt(w[i+1]-w[i]*w[i])
 			end
 			table.insert(out, { T, mu, table.unpack(w) })
 			--print(T, mu, table.unpack(w))
 		end
 	end
-	table.sort(out, function(x, y) if x[1]<y[1] then return true elseif x[1]==y[1] and x[2]<y[2] then return true else return false end end)
-	local T
-	for _, t in ipairs(out) do
-		if t[1]~=T then print() T = t[1] end
-		print(table.unpack(t))
-	end
+end
+table.sort(out, function(x, y) if x[2]<y[2] then return true elseif x[2]==y[2] and x[1]<y[1] then return true else return false end end)
+local T
+for _, t in ipairs(out) do
+	if t[2]~=T then print() T = t[2] end
+	print(table.unpack(t))
 end
 
