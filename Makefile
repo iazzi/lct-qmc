1759c23e01829d5a37df548e532c3da550273bdb
Mauro Iazzi <mauro.iazzi@gmail.com>
2013-11-18 11:42:57 +0100

    setup the batch file for SLURM systems

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CXXFLAGS=$(MYCXXFLAGS) -std=c++11 -I $(HOME)/local/include `pkg-config --cflags 
 LDFLAGS=$(MYLDFLAGS) -L $(HOME)/local/lib `pkg-config --libs eigen3`
 LDLIBS=$(MYLDLIBS) -lgmp -lmpfr `pkg-config --libs eigen3` -lm -lstdc++ -lmkl_gf_lp64 -lmkl_scalapack_lp64 -lmkl_blacs_openmpi_lp64 -lmkl_sequential -lmkl_core -llua -pthread -lfftw3_threads -lfftw3 -lmpi
 
-all: full unstable main test_params
+all: full unstable main test_params setup_batch
 
 main: main.o simulation.o mpfr.o
 
@@ -11,6 +11,8 @@ simulation.o: simulation.cpp simulation.hpp
 main.o: main.cpp simulation.hpp
 
 test_params: test_params.o simulation.o mpfr.o
+
+setup_batch: setup_batch.o simulation.o mpfr.o
 
 parallel:
 	$(MAKE) all MYCXXFLAGS="-O3 -march=native -DNDEBUG -DEIGEN_NO_DEBUG -fopenmp" MYLDFLAGS="-fopenmp -lfftw3_threads"
---
+++ setup_batch.cpp
@@ -0,0 +1,144 @@
+#include "simulation.hpp"
+#include "logger.hpp"
+
+#include <set>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <atomic>
+#include <sstream>
+
+using namespace std;
+using namespace std::chrono;
+
+typedef std::chrono::duration<double> seconds_type;
+
+string time_str (int t) {
+	stringstream str;
+	str << (t/3600) << ':' << (t/60%60) << ':' << (t%60);
+	return str.str();
+}
+
+int main (int argc, char **argv) {
+	lua_State *L = luaL_newstate();
+	luaL_openlibs(L);
+	if (luaL_dofile(L, argv[1])) {
+		std::cerr << "Error loading configuration file \"" << argv[1] << "\":" << std::endl;
+		std::cerr << '\t' << lua_tostring(L, -1) << std::endl;
+		return -1;
+	}
+
+	int nthreads = 1;
+	char *e = getenv("LSB_HOSTS");
+	while (e!=NULL && *e!='\0') if (*(e++)==' ') nthreads++;
+	e = getenv("SLURM_NPROCS");
+	if (e) {
+		string f(e);
+		if (!f.empty()) {
+			stringstream g(f);
+			g >> nthreads;
+		}
+	}
+
+	lua_getfield(L, -1, "THREADS");
+	if (lua_tointeger(L, -1)) {
+		nthreads = lua_tointeger(L, -1);
+	}
+	lua_pop(L, 1);
+	Logger log;
+
+	std::atomic<int> current;
+	current = 1;
+	double eta = 0.0;
+	lua_len(L, -1);
+	int njobs = lua_tonumber(L, -1);
+	lua_pop(L, 1);
+	std::vector<double> times;
+	times.resize(njobs);
+	for (int job=1;job<=njobs;job++) {
+		steady_clock::time_point t0 = steady_clock::now();
+		steady_clock::time_point t1 = steady_clock::now();
+			double ups, mps;
+			lua_rawgeti(L, -1, job);
+			if (lua_isnil(L, -1)) {
+				log << "job" << job << "empty: terminating";
+				break;
+			}
+			log << "running simulation" << job << '/' << njobs;
+			Simulation simulation(L, -1);
+			lua_getfield(L, -1, "THERMALIZATION"); int thermalization_sweeps = lua_tointeger(L, -1); lua_pop(L, 1);
+			lua_getfield(L, -1, "SWEEPS"); int total_sweeps = lua_tointeger(L, -1); lua_pop(L, 1);
+			lua_pop(L, 1);
+			int updates = 0;
+			t0 = steady_clock::now();
+			while (duration_cast<seconds_type>(steady_clock::now()-t0).count()<10) {
+				simulation.update();
+				updates++;
+			}
+			t1 = steady_clock::now();
+			ups = (double(updates)/duration_cast<seconds_type>(t1-t0).count());
+			log << ups << "updates per second";
+			int measures = 0;
+			t0 = steady_clock::now();
+			while (duration_cast<seconds_type>(steady_clock::now()-t0).count()<10) {
+				simulation.measure();
+				measures++;
+			}
+			t1 = steady_clock::now();
+			mps = (double(measures)/duration_cast<seconds_type>(t1-t0).count());
+			log << mps << "measures per second";
+			double jeta = (thermalization_sweeps+total_sweeps)/ups + total_sweeps/mps;
+			log << "estimated time:" << jeta;
+			eta += jeta;
+			times[job-1] = eta;
+	}
+	lua_close(L);
+	struct Batch {
+		double eta;
+		set<int> jobs;
+		bool operator< (const Batch& other) const {
+			return eta < other.eta;
+		}
+		Batch (double t) : eta(t) {}
+		Batch (double t, int j) : eta(t) { jobs.insert(j); }
+	};
+	set<Batch> batches;
+	for (int j=0;j<njobs;j++) {
+		for (auto b : batches) {
+			if (b.eta+times[j]<18*3600) {
+				b.eta += times[j];
+				times[j] = 0.0;
+				b.jobs.insert(j);
+				break;
+			}
+		}
+		if (times[j]>0.0) {
+			set<Batch>::iterator b = batches.insert(Batch(times[j], j)).first;
+				times[j] = 0.0;
+		}
+	}
+	for (auto b : batches) {
+		cout << "eta " << b.eta << "; jobs [";
+		for (auto j: b.jobs) cout << j << ", ";
+		cout << "]\n";
+	}
+	log << "total estimated time:" << time_str(eta);
+	string outname(argv[1]);
+	outname.append(".batch");
+	ofstream out(outname);
+	out <<
+		"#!/usr/bin/bash -l\n"
+		"#\n"
+		"#SBATCH --job-name=\"test\"\n"
+		"#SBATCH --partition=dphys_compute\n"
+		"#SBATCH --cpus-per-task=20\n"
+		//"#SBATCH --nodes=1\n"
+		//"#SBATCH --ntasks-per-node=1\n"
+		"#SBATCH --output=outtest.%j.out\n"
+		"#SBATCH --error=errtest.%j.err\n";
+	out <<  "#SBATCH --time=" << time_str(2*eta/20) << "\n";
+	out << "./main " << argv[1] << "\n";
+	return 0;
+}
+
+
