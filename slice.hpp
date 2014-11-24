5edcbd4c067302868b7dd1f92466a13cbe3a2d5b
Mauro Iazzi <iazzi@itp.phys.ethz.ch>
2014-11-24 16:04:59 +0100

    fill in parts of Slice

--- slice.hpp
+++ slice.hpp
@@ -2,16 +2,52 @@
 #define SLICE_HPP
 
 #include <set>
+#include <Eigen/Dense>
 
-template <typename Interaction>
+template <typename Model>
 class Slice {
 	public:
+	typedef typename Model::Lattice Lattice;
+	typedef typename Model::Interaction Interaction;
 	typedef typename Interaction::Vertex Vertex;
 	private:
+	//Lattice &L;
 	Interaction &I;
+
 	std::set<Vertex, typename Vertex::Compare> verts;
+
+	size_t N;
+	double beta;
+
+	Eigen::MatrixXd matrix_;
+	Eigen::MatrixXd matrix_inv_;
+
 	public:
-	Slice (Interaction &i) : I(i) {}
+	Slice (Interaction &i) : I(i), N(i.volume()) {}
+
+	void setup (double b) {
+		beta = b;
+	}
+
+	void insert (const Vertex &v) { verts.insert(v); }
+	void clear () { verts.clear(); }
+
+	Eigen::MatrixXd matrix () {
+		matrix_.setIdentity(N, N);
+		double t0 = 0.0;
+		for (auto v : verts) {
+			I.apply_vertex_on_the_left(v, matrix_);
+		}
+		return matrix_;
+	}
+
+	Eigen::MatrixXd inverse () {
+		matrix_inv_.setIdentity(N, N);
+		for (auto v : verts) {
+			I.apply_inverse_on_the_left(v, matrix_inv_);
+		}
+		return matrix_inv_;
+	}
 };
 
 
