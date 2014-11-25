6cf69c9edd5a756e28cabdc257d0e9d54566669b
Mauro Iazzi <iazzi@itp.phys.ethz.ch>
2014-11-25 11:45:42 +0100

    function to multiply slices in a configuration

--- configuration.hpp
+++ configuration.hpp
@@ -1,6 +1,7 @@
 #ifndef CONFIGURATION_HPP
 #define CONFIGURATION_HPP
 
+#include "svd.hpp"
 #include <vector>
 #include "slice.hpp"
 
@@ -20,9 +21,20 @@ class Configuration {
 
 		double beta;
 		double dtau;
-		double M;
+		size_t M;
+
+		SVDHelper svd;
+		size_t index;
+
+		void compute () {
+			svd.setIdentity(model.lattice().volume()); // FIXME: amybe have a direct reference to the lattice here too
+			for (size_t i=0;i<M;i++) {
+				svd.U.applyOnTheLeft(slices[(i+index)%M].matrix()); // FIXME: apply directly from the slice rather than multiplying the temporary
+				svd.absorbU(); // FIXME: have a random matrix applied here possibly only when no vertices have been applied
+			}
+		}
 	public:
-		Configuration (std::mt19937_64 &g, Model &m) : generator(g), model(m) {}
+		Configuration (std::mt19937_64 &g, Model &m) : generator(g), model(m), index(0) {}
 		void setup (double b, size_t m) {
 			beta = b;
 			M = m;
