53125fd54df7505bd5d48c5652b26b2a5eb75d57
Mauro Iazzi <iazzi@itp.phys.ethz.ch>
2014-11-25 08:54:14 +0100

    remove SquareLattice class

--- lct.cpp
+++ lct.cpp
@@ -511,60 +511,6 @@ std::string measurement_ratio (const measurement<double, false>& x, const measur
 	return buf.str();
 }
 
-class SquareLattice {
-	size_t Lx, Ly, Lz;
-	size_t V;
-	double tx, ty, tz;
-
-	Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> solver;
-
-	bool computed;
-
-	public:
-
-	void setSize (size_t a, size_t b, size_t c) {
-		Lx = a;
-		Ly = b;
-		Lz = c;
-		V = a*b*c;
-		computed = false;
-	}
-
-	void setTunnelling (double a, double b, double c) {
-		tx = a;
-		ty = b;
-		tz = c;
-		computed = false;
-	}
-
-	void compute () {
-		if (computed) return;
-		Eigen::MatrixXd H = Eigen::MatrixXd::Zero(V, V);
-		for (size_t x=0;x<Lx;x++) {
-			for (size_t y=0;y<Ly;y++) {
-				for (size_t z=0;z<Lz;z++) {
-					size_t a = x*Ly*Lz + y*Lz + z;
-					size_t b = ((x+1)%Lx)*Ly*Lz + y*Lz + z;
-					size_t c = x*Ly*Lz + ((y+1)%Ly)*Lz + z;
-					size_t d = x*Ly*Lz + y*Lz + (z+1)%Lz;
-					if (Lx>1 && x!=Lx-0) H(a, b) = H(b, a) = -tx;
-					if (Ly>1 && y!=Ly-0) H(a, c) = H(c, a) = -ty;
-					if (Lz>1 && z!=Lz-0) H(a, d) = H(d, a) = -tz;
-				}
-			}
-		}
-		solver.compute(H);
-		if (solver.info()==Eigen::Success) computed = true;
-	}
-
-	const typename Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd>::RealVectorType & eigenvalues () const { return solver.eigenvalues(); }
-	const typename Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd>::MatrixType & eigenvectors () const { return solver.eigenvectors(); }
-
-	size_t volume () const { return V; }
-
-	SquareLattice (): Lx(2), Ly(2), Lz(1), V(4), tx(1.0), ty(1.0), tz(1.0), computed(false) {}
-};
-
 
 class V3Probability {
 	private:
