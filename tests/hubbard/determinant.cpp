33c3a690c2ad7a26f55b8eadf2f967b6c32ac441
Mauro Iazzi <iazzi@itp.phys.ethz.ch>
2014-12-15 16:21:05 +0100

    use new style for inserting vertices

--- tests/hubbard/determinant.cpp
+++ tests/hubbard/determinant.cpp
@@ -30,8 +30,12 @@ int main () {
 	auto model = make_model(lattice, interaction);
 	Configuration<Model<CubicLattice, HubbardInteraction>> conf(generator, model);
 	conf.setup(20.0, 0.0, N); // beta, mu (relative to half filling), slice number
-	for (int i=0;i<20*L*L;i++) {
-		conf.insert(interaction.generate(0.0, 20.0));
+	for (size_t i=0;i<conf.slice_number();i++) {
+		conf.set_index(i);
+		for (size_t j=0;j<L*L;j++) {
+			conf.insert(interaction.generate(0.0, conf.slice_end()-conf.slice_start()));
+		}
+		//std::cerr << i << " -> " << conf.slice_size() << std::endl;
 	}
 	for (int i=0;i<N;i++) {
 		conf.set_index(i);
