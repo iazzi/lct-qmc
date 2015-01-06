784092c3e71e2b54dedb097e92de2d4b6c211152
Mauro Iazzi <iazzi@itp.phys.ethz.ch>
2015-01-06 14:22:05 +0100

    updated wrap.cpp and metropolis.cpp

--- tests/hubbard/metropolis.cpp
+++ tests/hubbard/metropolis.cpp
@@ -3,6 +3,7 @@
 #include "slice.hpp"
 #include "model.hpp"
 #include "hubbard.hpp"
+#include "spin_one_half.hpp"
 
 #include <random>
 #include <iostream>
@@ -13,28 +14,27 @@
 using namespace std;
 using namespace Eigen;
 
-const int L = 4;
 int N = 80;
 
 double relative_error (double a, double b) {
 	return fabs(a-b)/min(fabs(a), fabs(b));
 }
 
-int main () {
+int main (int argc, char **argv) {
 	std::mt19937_64 generator;
+	Parameters params(argc, argv);
 	std::uniform_real_distribution<double> d;
 	std::exponential_distribution<double> trial;
-	CubicLattice lattice;
-	lattice.set_size(L, L, 1);
+	SpinOneHalf<CubicLattice> lattice(params);
 	lattice.compute();
 	HubbardInteraction interaction(generator);
 	interaction.setup(lattice.eigenvectors(), 4.0, 5.0);
 	auto model = make_model(lattice, interaction);
-	Configuration<Model<CubicLattice, HubbardInteraction>> conf(generator, model);
+	Configuration<Model<SpinOneHalf<CubicLattice>, HubbardInteraction>> conf(generator, model);
 	conf.setup(20.0, 0.0, N); // beta, mu (relative to half filling), slice number
 	for (size_t i=0;i<conf.slice_number();i++) {
 		conf.set_index(i);
-		for (size_t j=0;j<L*L;j++) {
+		for (size_t j=0;j<lattice.volume();j++) {
 			conf.insert(interaction.generate(0.0, conf.slice_end()-conf.slice_start()));
 		}
 		//std::cerr << i << " -> " << conf.slice_size() << std::endl;
@@ -47,7 +47,7 @@ int main () {
 		conf.compute_G();
 		conf.save_G();
 		double p1 = conf.probability().first;
-		for (int j=0;j<L*L;j++) {
+		for (int j=0;j<lattice.volume();j++) {
 			double dp = 0.0;
 			if (d(generator)<0.5) {
 				v = conf.get_vertex(d(generator)*conf.slice_size());
