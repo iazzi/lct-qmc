507666cefdf454ad19e4a5db7881c6fc4bbb1b3a
Mauro Iazzi <iazzi@itp.phys.ethz.ch>
2015-02-10 20:43:33 +0100

    add function for sweep

--- tests/hubbard/kinetic2.cpp
+++ tests/hubbard/kinetic2.cpp
@@ -47,89 +47,70 @@ int main (int argc, char **argv) {
 	ofstream diff ("diff.dat", ios::app);
 	diff << "\"V=" << model.interaction().dimension() << " beta=" << conf.inverse_temperature() << "\"" << endl;
 	conf.compute_B();
+	conf.compute_G();
+	conf.save_G();
 	double p1 = conf.probability().first;
 	double pr = 0.0;
+	auto sweep = [&conf, &d, &trial, &pr, &model] (mt19937_64 &generator) {
+		HubbardInteraction::Vertex v;
+		for (size_t j=0;j<model.lattice().volume();j++) {
+			double dp = 0.0;
+			if (d(generator)<0.5) {
+				v = conf.get_vertex(d(generator)*conf.slice_size());
+				dp = std::log(std::fabs(conf.remove_probability(v)));
+				if (-trial(generator)<dp+conf.remove_factor()) {
+					//cerr << "removed vertex " << v.tau << endl;
+					conf.remove_and_update(v);
+					pr += dp;
+				} else {
+					//cerr << "remove rejected" << endl;
+				}
+			} else {
+				v = model.interaction().generate(0.0, conf.slice_end()-conf.slice_start(), generator);
+				dp = std::log(std::fabs(conf.insert_probability(v)));
+				if (-trial(generator)<dp+conf.insert_factor()) {
+					//cerr << "inserted vertex " << v.tau << endl;
+					conf.insert_and_update(v);
+					pr += dp;
+				} else {
+					//cerr << "insert rejected" << endl;
+				}
+			}
+		}
+		conf.compute_right_side();
+	};
 	Eigen::MatrixXd G;
 	for (size_t i=0;i<conf.slice_number();i++) {
-		conf.compute_right_side();
 		conf.set_index(i);
 		conf.compute_right_side();
+		//conf.compute_B();
+		//conf.compute_G();
+		//conf.save_G();
 		conf.compute_propagators_2();
 		G = conf.green_function();
 		//std::cerr << G << std::endl << std::endl;
-		conf.compute_B();
-		conf.compute_G();
-		conf.save_G();
 		cerr << (double(i)/conf.slice_number()) << ' '
 			<< conf.inverse_temperature() << ' '
 			<< model.interaction().dimension() << ' '
 			<< (conf.green_function()-G).norm() << ' '
 			<< (conf.green_function()-G).cwiseAbs().maxCoeff() << endl;
-		HubbardInteraction::Vertex v;
-		for (size_t j=0;j<lattice.volume();j++) {
-			double dp = 0.0;
-			if (d(generator)<0.5) {
-				v = conf.get_vertex(d(generator)*conf.slice_size());
-				dp = std::log(std::fabs(conf.remove_probability(v)));
-				if (-trial(generator)<dp+conf.remove_factor()) {
-					//cerr << "removed vertex " << v.tau << endl;
-					conf.remove_and_update(v);
-					pr += dp;
-				} else {
-					//cerr << "remove rejected" << endl;
-				}
-			} else {
-				v = model.interaction().generate(0.0, conf.slice_end()-conf.slice_start(), generator);
-				dp = std::log(std::fabs(conf.insert_probability(v)));
-				if (-trial(generator)<dp+conf.insert_factor()) {
-					//cerr << "inserted vertex " << v.tau << endl;
-					conf.insert_and_update(v);
-					pr += dp;
-				} else {
-					//cerr << "insert rejected" << endl;
-				}
-			}
-		}
+		sweep(generator);
 	}
 	for (size_t i=conf.slice_number();i>0;i--) {
 		conf.set_index(i-1);
 		conf.compute_left_side();
+		//std::cerr << G << std::endl << std::endl;
+		//conf.compute_B();
+		//conf.compute_G();
+		//conf.save_G();
 		conf.compute_propagators_2();
 		G = conf.green_function();
-		//std::cerr << G << std::endl << std::endl;
-		conf.compute_B();
-		conf.compute_G();
-		conf.save_G();
 		cerr << (double(i-1)/conf.slice_number()) << ' '
 			<< conf.inverse_temperature() << ' '
 			<< model.interaction().dimension() << ' '
 			<< (conf.green_function()-G).norm() << ' '
 			<< (conf.green_function()-G).cwiseAbs().maxCoeff() << endl;
-		HubbardInteraction::Vertex v;
-		for (size_t j=0;j<lattice.volume();j++) {
-			double dp = 0.0;
-			if (d(generator)<0.5) {
-				v = conf.get_vertex(d(generator)*conf.slice_size());
-				dp = std::log(std::fabs(conf.remove_probability(v)));
-				if (-trial(generator)<dp+conf.remove_factor()) {
-					//cerr << "removed vertex " << v.tau << endl;
-					conf.remove_and_update(v);
-					pr += dp;
-				} else {
-					//cerr << "remove rejected" << endl;
-				}
-			} else {
-				v = model.interaction().generate(0.0, conf.slice_end()-conf.slice_start(), generator);
-				dp = std::log(std::fabs(conf.insert_probability(v)));
-				if (-trial(generator)<dp+conf.insert_factor()) {
-					//cerr << "inserted vertex " << v.tau << endl;
-					conf.insert_and_update(v);
-					pr += dp;
-				} else {
-					//cerr << "insert rejected" << endl;
-				}
-			}
-		}
+		sweep(generator);
 	}
 	diff << endl << endl;
 	conf.compute_B();
