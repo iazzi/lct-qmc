dd7d03bfefa954d79506c15b6efdb7e706856f02
Mauro Iazzi <mauro.iazzi@gmail.com>
2015-08-19 16:37:08 +0200

    use updates from the right edge

--- lctsimulation.hpp
+++ lctsimulation.hpp
@@ -55,10 +55,10 @@ class LCTSimulation {
 			p1 = 0.0, ps = 0.0, pr = 0.0;
 			std::tie(p1, ps) = conf.probability();
 			conf.set_index(0);
-			conf.compute_propagators_2();
+			conf.compute_propagators_2_right();
 		}
 
-	void update (bool check = false) {
+	void update_left (bool check = false) {
 		HubbardInteraction::Vertex v;
 		double dp = 0.0, s = 1.0;
 		if (d(generator)<0.5) {
@@ -97,42 +97,86 @@ class LCTSimulation {
 		updates_++;
 	}
 
+	void update_right (bool check = false) {
+		HubbardInteraction::Vertex v;
+		double dp = 0.0, s = 1.0;
+		if (d(generator)<0.5) {
+			v = conf.get_vertex(d(generator)*conf.slice_size());
+			dp = conf.remove_probability_right(v);
+			s = dp>0.0?1.0:-1.0;
+			dp = std::log(std::fabs(dp));
+			if (-trial(generator)<dp+conf.remove_factor()) {
+				//cerr << "removed vertex " << v.tau << endl;
+				conf.remove_and_update_right(v);
+				pr += dp;
+				ps *= s;
+			} else {
+				//cerr << "remove rejected" << endl;
+			}
+		} else {
+			v = conf.generate_vertex(generator);
+			dp = conf.insert_probability_right(v);
+			s = dp>0.0?1.0:-1.0;
+			dp = std::log(std::fabs(dp));
+			if (-trial(generator)<dp+conf.insert_factor()) {
+				//cerr << "inserted vertex " << v.tau << endl;
+				conf.insert_and_update_right(v);
+				pr += dp;
+				ps *= s;
+			} else {
+				//cerr << "insert rejected" << endl;
+			}
+		}
+		if (check) {
+			conf.compute_B();
+			double p2 = conf.probability().first;
+			//std::cerr << "v = " << v.x << ',' << v.tau << " dp = " << p1+pr-p2 << ' ' << p2-p1 << ' ' << pr << std::endl << endl;
+		}
+		//conf.compute_right_side(conf.current_slice()+1);
+		updates_++;
+	}
+
 	void prepare () {
+		if (is_direction_right_to_left()) {
+		} else {
+		}
 	}
 
 	void sweep (bool check = false) {
 		HubbardInteraction::Vertex v;
-		for (size_t j=0;j<conf.volume();j++) {
-			update(check);
+		if (is_direction_right_to_left()) {
+			for (size_t j=0;j<conf.volume();j++) {
+				update_right(check);
+			}
+		} else {
+			for (size_t j=0;j<conf.volume();j++) {
+				update_left(check);
+			}
 		}
 		//conf.compute_right_side(conf.current_slice()+1);
-		if ( (is_direction_right_to_left())
-				|| (false && is_direction_left_to_right() && conf.current_slice()==0) ) {
-			conf.compute_right_side(conf.current_slice()+1);
-		}
 	}
 
 	void next () {
 		if (is_direction_right_to_left()) {
 			if (conf.current_slice()+1<conf.slice_number()) {
-				conf.set_index(conf.current_slice()+1);
 				conf.compute_right_side(conf.current_slice()+1);
-				conf.compute_propagators_2();
+				conf.set_index(conf.current_slice()+1);
+				conf.compute_propagators_2_right();
 			} else {
 				//conf.set_index(conf.current_slice()-1);
-				conf.compute_left_side(conf.current_slice()+1);
+				conf.compute_right_side(conf.current_slice()+1);
 				conf.compute_propagators_2();
 				set_direction_left_to_right();
 			}
 		} else {
 			if (conf.current_slice()>0) {
+				conf.compute_left_side(conf.current_slice());
 				conf.set_index(conf.current_slice()-1);
-				conf.compute_left_side(conf.current_slice()+1);
 				conf.compute_propagators_2();
 			} else {
 				//conf.set_index(conf.current_slice()+1);
-				conf.compute_right_side(conf.current_slice()+1);
-				conf.compute_propagators_2();
+				conf.compute_left_side(conf.current_slice());
+				conf.compute_propagators_2_right();
 				set_direction_right_to_left();
 			}
 		}
@@ -164,6 +208,8 @@ class LCTSimulation {
 
 	double probability () const { return p1+pr; }
 	double sign () const { return ps; }
+	double exact_probability () { conf.compute_B(); return conf.probability().first; }
+	double probability_difference () const { return pr; }
 
 	size_t vertices () const { return conf.vertices(); }
 	const Eigen::MatrixXd & green_function () const {
@@ -187,8 +233,6 @@ class LCTSimulation {
 	void set_sweep_direction (sweep_direction_type d) { sweep_direction_ = d; }
 	void set_direction_left_to_right () { sweep_direction_ = left_to_right; }
 	void set_direction_right_to_left () { sweep_direction_ = right_to_left; }
-
-	double exact_probability () { conf.compute_B(); return conf.probability().first; }
 };
 
 #endif // LCTSIMULATION
