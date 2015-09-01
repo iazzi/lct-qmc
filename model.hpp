43e3212872b7304412dfe23d868a79c438a812e7
Mauro Iazzi <mauro.iazzi@gmail.com>
2015-09-01 16:48:01 +0200

    remove some more references to the lattice

--- model.hpp
+++ model.hpp
@@ -5,17 +5,16 @@
 
 template <class L, class I> 
 class Model {
-	L l;
 	I i;
 	public:
-	Model (const Model &m) : l(m.l), i(m.i) { i.set_lattice_eigenvectors(l.eigenvectors()); i.set_lattice_eigenvalues(l.eigenvalues()); }
-	Model (const L &a, const I &b) : l(a), i(b) { i.set_lattice_eigenvectors(l.eigenvectors()); i.set_lattice_eigenvalues(l.eigenvalues()); }
-	Model (const Parameters &params) : l(params), i(params) { i.set_lattice_eigenvectors(l.eigenvectors()); i.set_lattice_eigenvalues(l.eigenvalues()); }
+	Model (const Model &m) : i(m.i) { i.set_lattice_eigenvectors(i.eigenvectors()); i.set_lattice_eigenvalues(i.eigenvalues()); }
+	Model (const I &b) : i(b) { i.set_lattice_eigenvectors(i.eigenvectors()); i.set_lattice_eigenvalues(i.eigenvalues()); }
+	Model (const Parameters &params) : i(params) { L l(params); i.set_lattice_eigenvectors(l.eigenvectors()); i.set_lattice_eigenvalues(l.eigenvalues()); }
 	I& interaction () { return i; }
 	const I& interaction () const { return i; }
 	typedef I Interaction;
-	template <typename T> void propagate (double t, const T &M) { l.propagate(t, M); }
-	template <typename T> void propagate_right (double t, const T &M) { l.propagate_right(t, M); }
+	template <typename T> void propagate (double t, const T &M) { i.propagate(t, M); }
+	template <typename T> void propagate_right (double t, const T &M) { i.propagate_right(t, M); }
 };
 
 template <class L, class I> 
