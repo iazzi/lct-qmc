fe9e5562fd5124918b54b84881e7acecf9919f7e
Mauro Iazzi <mauro.iazzi@gmail.com>
2015-09-01 14:20:49 +0200

    pass eigenvalues to the interaction

--- model.hpp
+++ model.hpp
@@ -8,9 +8,9 @@ class Model {
 	L l;
 	I i;
 	public:
-	Model (const Model &m) : l(m.l), i(m.i) { i.set_lattice_eigenvectors(l.eigenvectors()); }
-	Model (const L &a, const I &b) : l(a), i(b) { i.set_lattice_eigenvectors(l.eigenvectors()); }
-	Model (const Parameters &params) : l(params), i(params) { i.set_lattice_eigenvectors(l.eigenvectors()); }
+	Model (const Model &m) : l(m.l), i(m.i) { i.set_lattice_eigenvectors(l.eigenvectors()); i.set_lattice_eigenvalues(l.eigenvalues()); }
+	Model (const L &a, const I &b) : l(a), i(b) { i.set_lattice_eigenvectors(l.eigenvectors()); i.set_lattice_eigenvalues(l.eigenvalues()); }
+	Model (const Parameters &params) : l(params), i(params) { i.set_lattice_eigenvectors(l.eigenvectors()); i.set_lattice_eigenvalues(l.eigenvalues()); }
 	L& lattice () { return l; }
 	const L& lattice () const { return l; }
 	I& interaction () { return i; }
