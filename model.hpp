1cb5f69cb3740ce38aeb61319ea105049a7bab85
Mauro Iazzi <mauro.iazzi@gmail.com>
2015-09-03 20:34:44 +0200

    keep a reference to the lattice in the model (for performance)

--- model.hpp
+++ model.hpp
@@ -5,12 +5,12 @@
 
 template <class L, class I> 
 class Model {
+	L l;
 	I i;
 	public:
-	Model (const Model &m) : i(m.i) { i.set_lattice_eigenvectors(i.eigenvectors()); i.set_lattice_eigenvalues(i.eigenvalues()); }
-	Model (const L&a, const I &b) : i(b) { i.set_lattice_eigenvectors(b.eigenvectors()); i.set_lattice_eigenvalues(b.eigenvalues()); }
-	Model (const I &b) : i(b) { i.set_lattice_eigenvectors(i.eigenvectors()); i.set_lattice_eigenvalues(i.eigenvalues()); }
-	Model (const Parameters &params) : i(params) { L l(params); i.set_lattice_eigenvectors(l.eigenvectors()); i.set_lattice_eigenvalues(l.eigenvalues()); }
+	//Model (const Model &m) : l(m.l), i(m.i) { i.set_lattice_eigenvectors(l.eigenvectors()); i.set_lattice_eigenvalues(l.eigenvalues()); }
+	Model (const L &a, const I &b) : i(b) { i.set_lattice_eigenvectors(a.eigenvectors()); i.set_lattice_eigenvalues(a.eigenvalues()); }
+	Model (const Parameters &params) : l(params), i(params) { i.set_lattice_eigenvectors(l.eigenvectors()); i.set_lattice_eigenvalues(l.eigenvalues()); }
 	I& interaction () { return i; }
 	const I& interaction () const { return i; }
 	typedef I Interaction;
