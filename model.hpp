ddb6a66d3855ba9ff9b10b5483d6d10dd8c3c874
Mauro Iazzi <mauro.iazzi@gmail.com>
2015-09-07 11:14:04 +0200

    only build the lattice on the stack

--- model.hpp
+++ model.hpp
@@ -5,12 +5,11 @@
 
 template <class L, class I> 
 class Model {
-	L l;
 	I i;
 	public:
 	//Model (const Model &m) : l(m.l), i(m.i) { i.set_lattice_eigenvectors(l.eigenvectors()); i.set_lattice_eigenvalues(l.eigenvalues()); }
 	Model (const L &a, const I &b) : i(b) { i.set_lattice_eigenvectors(a.eigenvectors()); i.set_lattice_eigenvalues(a.eigenvalues()); }
-	Model (const Parameters &params) : l(params), i(params) { i.set_lattice_eigenvectors(l.eigenvectors()); i.set_lattice_eigenvalues(l.eigenvalues()); }
+	Model (const Parameters &params) : i(params) { L l(params); i.set_lattice_eigenvectors(l.eigenvectors()); i.set_lattice_eigenvalues(l.eigenvalues()); }
 	I& interaction () { return i; }
 	const I& interaction () const { return i; }
 	typedef I Interaction;
