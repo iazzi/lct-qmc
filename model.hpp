3434a06b24256b9be9f7e43d6b939f2d30d2e6fc
Mauro Iazzi <mauro.iazzi@gmail.com>
2015-09-09 15:06:56 +0200

    Model does not use lattice anymore (only forward)
    Model will be removed soon

--- model.hpp
+++ model.hpp
@@ -8,8 +8,8 @@ class Model {
 	I i;
 	public:
 	//Model (const Model &m) : l(m.l), i(m.i) { i.set_lattice_eigenvectors(l.eigenvectors()); i.set_lattice_eigenvalues(l.eigenvalues()); }
-	Model (const L &a, const I &b) : i(b) { i.set_lattice_eigenvectors(a.eigenvectors()); i.set_lattice_eigenvalues(a.eigenvalues()); }
-	Model (const Parameters &params) : i(params) { L l(params); i.set_lattice_eigenvectors(l.eigenvectors()); i.set_lattice_eigenvalues(l.eigenvalues()); }
+	//Model (const L &a, const I &b) : i(b) { i.set_lattice_eigenvectors(a.eigenvectors()); i.set_lattice_eigenvalues(a.eigenvalues()); }
+	Model (const Parameters &params) : i(params) {}
 	I& interaction () { return i; }
 	const I& interaction () const { return i; }
 	typedef I Interaction;
